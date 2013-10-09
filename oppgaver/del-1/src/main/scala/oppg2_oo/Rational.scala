f6287c786850c33b27bb302bd3cf6c83bfe59a96
Nina Margrethe Smørsgård <nina.m.smorsgard@gmail.com>
2013-10-09 18:56:31 +0200

    start working on oo

--- oppgaver/del-1/src/main/scala/oppg2_oo/Rational.scala
+++ oppgaver/del-1/src/main/scala/oppg2_oo/Rational.scala
@@ -1,6 +1,6 @@
 package oppg2_oo
 
-class Rational {
+class Rational(n:Int, d:Int) {
 
   /**
    * Remember that the body of the class is the body of the primary constructor. Assignments goes here.
@@ -8,8 +8,8 @@ class Rational {
    * ??? Throws a NotImplementedException and returns the type Nothing
    * which is a subtype of every other type in Scala
    */
-  val numer: Int = ???
-  val denom: Int = ???
+  val numer: Int = 1
+  val denom: Int = if(d==0) throw new IllegalArgumentException else d
 
   /**
    * Hint: 1   2   3   4
@@ -17,18 +17,22 @@ class Rational {
    *       2   3   6   6
    * You don't need to find the greatest common divisor
    */
-  def add(that: Rational): Rational = ???
+  def add(that: Rational): Rational = {
+    val div =  this.denom * Rational.???
+  }
+
+  override def toString = this.numer.toString+"/"+this.denom
 }
 
 object Rational {
   /**
    * Hint: Create and invoke a constructor on class Rational
    */
-  def apply(n: Int, d: Int): Rational = ???
+  def apply(n: Int, d: Int): Rational = new Rational(n,d)
 
   /**
    * Hint: Don't call the other apply method
    * Create a separate constructor using 'this'
    */
-  def apply(n: Int): Rational = ???
+  def apply(n: Int): Rational = this(n,1)
 }
